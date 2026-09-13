e85ba653013130bfaa707fd16ad5732b841756e1
Bakhtawar Fatima <bakhtawarfatima34q@gmail.com>
2026-09-13 20:25:20 +0500

    Time: 52 ms (53.14%) | Memory: 13 MB (68.83%) - LeetSync

---
+++ 864-image-overlap/image-overlap.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        
+        int n = img1.size();
+        int maxCount = 0;
+
+        for(int x = -(n-1); x <= n-1; x++) {
+            for(int y = -(n-1); y <= n-1; y++) {
+                
+                int count = 0;
+
+                for(int i = 0; i < n; i++) {
+                    for(int j = 0; j < n; j++) {
+
+                        int ni = i + x;
+                        int nj = j + y;
+
+                        if(ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                            if(img1[i][j] == 1 && img2[ni][nj] == 1) {
+                                count++;
+                            }
+                        }
+                    }
+                }
+
+                maxCount = max(maxCount, count);
+            }
+        }
+
+        return maxCount;
+    }
+};
